b5e728a9684ac66fcb0533ebff6d509aeea45b8b
coban-54 <92750818+coban-54@users.noreply.github.com>
2022-06-02 19:15:14 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisSeviceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoap clien1 = new KPSPublicSoapClient();
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrulaAsync(customer.NationalId, customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new System.DateTime(1995), FirstName = "Hüseyin", LastName = "Çoban", NationalId = 29257806470 });
+            
+        
+        }
+
+
+    }
+}
